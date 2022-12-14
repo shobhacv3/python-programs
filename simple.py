5c7c5b3c8dcc8430c5e274270b96fa1ce281b262
shobhacv3 <105140925+shobhacv3@users.noreply.github.com>
2022-12-14 08:37:38 +0530

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,7 @@
+m=int(input("enter first number:"))
+n=int(input("enter second number:"))
+op=input("enter operator:")
+if op=='+':
+  print("Addition of two numbers =",m+n)
+elif op=='-':
+  print("Subtraction of two numbers=",m-n)
